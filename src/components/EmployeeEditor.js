a278c778178aef23061b20c39c46ea13280f7945
= <webdev.jameslemire@gmail.com>
2017-05-09 09:19:47 -0600

    JL solution - Bind in constructor.

--- src/components/EmployeeEditor.js
+++ src/components/EmployeeEditor.js
@@ -8,6 +8,9 @@ class EmployeeEditor extends Component {
       originalEmployee: null,
       notModified: true
     };
+
+    this.save = this.save.bind( this );
+    this.cancel = this.cancel.bind( this );
   }
 
   componentWillReceiveProps(props) {
@@ -47,8 +50,8 @@ class EmployeeEditor extends Component {
             <span id="employeeID"> ID: { this.state.employee.id } </span>
             <p id="employeeTitle"> { this.state.originalEmployee.name } </p>
             <br />
-            <button id="saveBtn" className="confirmationButton" disabled={this.state.notModified} onClick={ this.save.bind(this) }> Save </button>
-            <button className="neutralButton" disabled={this.state.notModified} onClick={ this.cancel.bind(this) }> Cancel </button>
+            <button id="saveBtn" className="confirmationButton" disabled={this.state.notModified} onClick={ this.save }> Save </button>
+            <button className="neutralButton" disabled={this.state.notModified} onClick={ this.cancel }> Cancel </button>
             <br />
             <span className="placeholderText"> Name </span>
             <input className="materialInput" value={ this.state.employee.name } onChange={ (e) => { this.handleChange('name', e.target.value) } }></input>
